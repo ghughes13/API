dba24fae173ec9024c2ea5d0f2bccf53cc9af34e
Garrett <guitarguy13@ymail.com>
2020-03-18 16:03:02 -0500

    Added dailyToDo list with cron jobs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,8 +17,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-var cors=require('cors');
-
+const cors=require('cors');
 app.use(cors({origin:true,credentials: true}));
 
 mongoose.connect('mongodb+srv://adminGuy9er9er:AtlasShrugged@todolist900-qitpr.mongodb.net/test?retryWrites=true&w=majority', {
@@ -32,12 +31,19 @@ mongoose.connection.on('connected', () => {
 
 //Schema 
 const Schema = mongoose.Schema;
-const toDo = new Schema({
-  title:String
+
+const toDoListSchema = new Schema({
+  task:String
+});
+
+const dailyToDoSchema = new Schema({
+  task:String,
+  complete:Boolean
 });
 
 //Model
-const BlogPost = mongoose.model('toDoList', toDo)
+const toDoListModel = mongoose.model('toDoList', toDoListSchema)
+const dailyToDoListModel = mongoose.model('dailytodo', dailyToDoSchema)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -54,8 +60,8 @@ app.use('/users', usersRouter);
 
 
 //Custom Routes
-app.get('/getData', (req, res) => { //ROUTE TO GET INITIAL LIST FROM DB
-  BlogPost.find({ })
+app.get('/getToDoList', (req, res) => { //ROUTE TO GET INITIAL LIST FROM DB
+  toDoListModel.find({ })
   .exec()
   .then((data) => {
     res.json(data);
@@ -65,10 +71,33 @@ app.get('/getData', (req, res) => { //ROUTE TO GET INITIAL LIST FROM DB
   });
 })
 
+app.get('/getDailyToDoList', (req, res) => { //ROUTE TO GET INITIAL LIST FROM DB
+  dailyToDoListModel.find({ })
+  .exec()
+  .then((data) => {
+    console.log(data);
+    res.json(data);
+  })
+  .catch(error => {
+    console.log('error: ', error);
+  });
+})
+
+app.post('/addNewDaily', function(req, res) {  //ROUTE TO ADD NEW ITEM TO DAILY TODO
+  console.log(req.body.task, req.body.complete);
+  const newToDoItem = new dailyToDoListModel({ task: req.body.task, complete: req.body.complete });
+  newToDoItem.save((error) => {
+    if(error) {
+      console.log('error');
+    } else {
+      console.log('saved data!');
+    }
+  })
+  res.sendStatus(200);
+})
+
 app.post('/addNew', function(req, res) {  //ROUTE TO ADD NEW ITEM
-    
-    const newToDoItem = new BlogPost({ title: req.body.title });
-    
+    const newToDoItem = new toDoListModel({ task: req.body.task });
     newToDoItem.save((error) => {
       if(error) {
         console.log('error');
@@ -76,16 +105,12 @@ app.post('/addNew', function(req, res) {  //ROUTE TO ADD NEW ITEM
         console.log('saved data!');
       }
     })
-
     res.sendStatus(200);
 })
 
-
-
-
 app.delete('/delItem', function(req, res) {
-  BlogPost.findByIdAndDelete((req.body.delThis)).exec(() => {
-    BlogPost.find({ }).exec((err, data) => {
+  toDoListModel.findByIdAndDelete((req.body.delThis)).exec(() => {
+    toDoListModel.find({ }).exec((err, data) => {
       console.log(data);
       res.json(data);
       console.log('sent updated list')
@@ -93,15 +118,21 @@ app.delete('/delItem', function(req, res) {
   })
 })
 
-
-
-
 app.put('/updateItem', function(req, res) {
-  BlogPost.findByIdAndUpdate(req.body.editThis, { title: req.body.newText }).exec(() => {
-    BlogPost.find({ }).exec((err, data) => {
+  toDoListModel.findByIdAndUpdate(req.body.editThis, { task: req.body.newText }).exec(() => {
+    toDoListModel.find({ }).exec((err, data) => {
       console.log(data)
       res.json(data);
       console.log('sent response')
+    })
+  })
+})
+
+app.put('/updateDailyItem', function(req, res) {
+  console.log(req.body.editThis)
+  dailyToDoListModel.findByIdAndUpdate({ _id: req.body.editThis }, { complete: true }).exec(() => {
+    dailyToDoListModel.find({ }).exec((err, data) => {
+      res.json(data);
     })
   })
 })
@@ -121,5 +152,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+const resetDailyToDoList = () => {
+  dailyToDoListModel.find({ }).exec((err, data) => {
+    data.forEach(item => {
+      dailyToDoListModel.findByIdAndUpdate({ _id: item.id }, { complete: false }).exec();
+    })
+  })
+}
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob('0 5 * * *', resetDailyToDoList(), null, true, 'America/Chicago');
+job.start();
 
 module.exports = app;

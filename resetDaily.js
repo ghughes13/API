9d33ceedc8563679500cf186e9ac2d5e82337ef6
Garrett <guitarguy13@ymail.com>
2020-03-31 18:53:21 -0500

    added console log to see if it runs

--- resetDaily.js
+++ resetDaily.js
@@ -3,16 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'content-type');
-  next();
-});
-
-const cors=require('cors');
-app.use(cors({origin:true,credentials: true}));
-
+try {
 mongoose.connect('mongodb+srv://adminGuy9er9er:AtlasShrugged@todolist900-qitpr.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -36,6 +27,9 @@ dailyListModel.find({ }).exec((err, data) => {
   data.forEach(item => {
     dailyListModel.findByIdAndUpdate({ _id: item.id }, { complete: false }).exec();
   })
+  console.log('list reset')
 })
-
+} catch(error) {
+  console.log(error);
+}
 
